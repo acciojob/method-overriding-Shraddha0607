d2c1fd995a0dc4ece42012e4f54a8f818d1d5141
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2024-02-12 12:23:49 +0530

    override is added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,10 +5,14 @@ public class A{
         return "Invoking method from class A";
     }
 }
-public class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
+
+
+
+    @Override
+    public class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
 }
 public class Main {
   B obj = new B();
